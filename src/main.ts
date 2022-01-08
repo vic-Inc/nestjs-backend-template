75b6a3d2ac79b05ac6e5b30fa4a6f6124665cc19
vic <victor.pyatak@botscrew.com>
2022-01-08 19:03:47 +0200

    APP: [feat] adding compression and cors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { validationPipeOptions } from './app.configs';
 import { AppModule } from './app.module';
@@ -7,6 +8,7 @@ import { AppModule } from './app.module';
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
-
+  app.use(compression());
+  app.enableCors();
   await app.listen(process.env.PORT || 4000);
 })();
